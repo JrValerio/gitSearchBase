6e47ba081aaa96706918db4e603046adc2e04364
Amaro V S Júnior <amarovsjr81@gmail.com>
2024-01-19 17:55:51 -0300

    Git Searche

--- src/scripts/error.js
+++ src/scripts/error.js
@@ -1,6 +1,6 @@
 const newSearchButton = document.querySelector(".new-search__button");
 newSearchButton
   ? newSearchButton.addEventListener("click", () =>
-  window.location.replace("/index.html")
+  window.location.replace("../../index.html")
     )
   : console.error("Botão de nova pesquisa não encontrado");
